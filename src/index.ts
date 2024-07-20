f89fe911eb548d02a586a0ee38404d41cf046767
JaydenPahukula <jayden.pahukula@gmail.com>
2024-07-20 10:59:06 -0600

    beginning stuff

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello")
+import {parseSong} from './songutils'
+
+const root = document.getElementById('root');
+if (!(root instanceof HTMLDivElement)){
+    throw new Error("Could not find root div");
+}
+
+fetch('./song.json').then(async (response) => {
+    const data = await response.json();
+    root.appendChild(parseSong(data) || document.createTextNode('invalid song format'));
+});
