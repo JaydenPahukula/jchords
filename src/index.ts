308c5f627f2f07e56c546d8fe3bcecbedd4052e0
JaydenPahukula <jayden.pahukula@gmail.com>
2024-07-23 22:00:12 -0600

    set up firebase

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { registerCustomComponents } from "src/components/manifest";
 import { renderInitialContent } from "./pages/router";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import firebaseConfig from "./firebase.config.json";
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
 
 registerCustomComponents();
 
