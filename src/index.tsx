7bd8d56e826235f8de334e4d85260baccbb39e46
JaydenPahukula <jayden.pahukula@gmail.com>
2024-07-27 18:18:54 -0600

    add document root

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from 'src/App'
 import 'src/index.css'
 
-ReactDOM.createRoot(document.body).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = document.getElementById("root")
+if (root !== null){
+    ReactDOM.createRoot(root).render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+} else {
+    console.error("Could not find root div");
+}
