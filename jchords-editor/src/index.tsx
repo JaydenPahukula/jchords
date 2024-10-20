e40bc0c0d7bd5f5756842a38952a67f106bf83c9
JaydenPahukula <jayden.pahukula@gmail.com>
2024-10-20 15:23:03 -0600

    remove react strict mode

--- jchords-editor/src/index.tsx
+++ jchords-editor/src/index.tsx
@@ -1,18 +1,15 @@
-import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './components/app/app';
+import App from 'src/components/app/app';
+import store from 'src/redux/store';
 import './index.css';
-import store from './redux/store';
 
 const root = document.getElementById('root');
 if (root !== null) {
   ReactDOM.createRoot(root).render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>,
+    <Provider store={store}>
+      <App />
+    </Provider>,
   );
 } else {
   console.error('Could not find root div');
