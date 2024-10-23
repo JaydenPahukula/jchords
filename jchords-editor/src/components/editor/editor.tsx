84b5761860dce0bd387e89de36e1b2d10a2a99d0
JaydenPahukula <jayden.pahukula@gmail.com>
2024-10-22 19:00:26 -0600

    add scroll matching in editor

--- jchords-editor/src/components/editor/editor.tsx
+++ jchords-editor/src/components/editor/editor.tsx
@@ -1,18 +1,49 @@
 // @ts-ignore
 import { parseSong, renderSong } from 'chord-mark';
-import { ChangeEvent, ReactElement } from 'react';
+import { ChangeEvent, ReactElement, useEffect, useRef } from 'react';
 import { useAppDispatch, useAppSelector } from 'src/redux/hooks';
 import { selectCurrSong, updateSongSrc } from 'src/redux/slices/songdata';
 import './editor.css';
 import EditorLeftMenu from './leftmenu/editorleftmenu';
 
 export default function Editor(): ReactElement {
+  const srcRef = useRef<HTMLTextAreaElement>(null);
+  const previewRef = useRef<HTMLPreElement>(null);
+
+  // event listeners to match scrolling
+  useEffect(() => {
+    const getRatio = (a: HTMLTextAreaElement, b: HTMLPreElement): number =>
+      (a.scrollHeight - a.clientHeight) / (b.scrollHeight - b.clientHeight);
+    function srcHandler() {
+      if (srcRef.current !== null && previewRef.current !== null) {
+        previewRef.current.removeEventListener('scroll', previewHandler);
+        previewRef.current.scrollTop =
+          srcRef.current.scrollTop / getRatio(srcRef.current, previewRef.current);
+        window.requestAnimationFrame(() => {
+          previewRef.current?.addEventListener('scroll', previewHandler);
+        });
+      }
+    }
+    function previewHandler() {
+      if (srcRef.current !== null && previewRef.current !== null) {
+        srcRef.current.removeEventListener('scroll', srcHandler);
+        srcRef.current.scrollTop =
+          previewRef.current.scrollTop * getRatio(srcRef.current, previewRef.current);
+        window.requestAnimationFrame(() => {
+          srcRef.current?.addEventListener('scroll', srcHandler);
+        });
+      }
+    }
+    srcRef.current?.addEventListener('scroll', srcHandler);
+    previewRef.current?.addEventListener('scroll', previewHandler);
+  });
+
   const song = useAppSelector(selectCurrSong);
   const dispatch = useAppDispatch();
 
   const renderedSong = renderSong(parseSong(song.src ?? ''));
 
-  const updateSrc = (e: ChangeEvent<HTMLTextAreaElement>) =>
+  const srcOnChange = (e: ChangeEvent<HTMLTextAreaElement>) =>
     dispatch(updateSongSrc({ id: song.info.id, newSrc: e.target.value }));
 
   return (
@@ -25,20 +56,25 @@ export default function Editor(): ReactElement {
           </div>
           <textarea
             id="editor-src"
+            ref={srcRef}
             wrap="off"
             autoComplete="off"
             autoCorrect="off"
             autoCapitalize="off"
             spellCheck="false"
             value={song.src}
-            onChange={updateSrc}
+            onChange={srcOnChange}
           ></textarea>
         </div>
         <div id="editor-preview-section">
           <div className="editor-header-wrapper">
             <h3 className="editor-header">Preview</h3>
           </div>
-          <pre id="editor-preview" dangerouslySetInnerHTML={{ __html: renderedSong }}></pre>
+          <pre
+            id="editor-preview"
+            ref={previewRef}
+            dangerouslySetInnerHTML={{ __html: renderedSong }}
+          ></pre>
         </div>
       </div>
     </div>
