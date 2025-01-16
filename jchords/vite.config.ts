23e7648e85c3b69732abbb619fbc1d7b0d308e67
JaydenPahukula <jayden.pahukula@gmail.com>
2025-01-15 20:36:48 -0700

    scaffolding

---
+++ jchords/vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import deno from "@deno/vite-plugin";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [deno(), react()],
+});
---
+++ shared/firebase/app.ts
@@ -0,0 +1,6 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase.json";
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
