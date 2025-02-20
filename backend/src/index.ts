7689131e3b2943b0b24dd7cedbf46a1d92c43147
JaydenPahukula <jayden.pahukula@gmail.com>
2025-02-20 16:42:23 -0700

    host backend on jchords domain

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,37 +1,17 @@
-// /**
-//  * Import function triggers from their respective submodules:
-//  *
-//  * import {onCall} from "firebase-functions/v2/https";
-//  * import {onDocumentWritten} from "firebase-functions/v2/firestore";
-//  *
-//  * See a full list of supported triggers at https://firebase.google.com/docs/functions
-//  */
-
-// import { onRequest } from 'firebase-functions/v2/https';
-
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   console.log(request);
-//   response.send('Hello from Firebase!');
-// });
-
-// import functions from 'firebase-functions';
 import cors from 'cors';
 import express from 'express';
-import { https } from 'firebase-functions';
+import { onRequest } from 'firebase-functions/v2/https';
 
 const app = express();
 
 app.use(cors({ origin: true }));
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   return res.status(200).send(':)');
 });
 
-app.get('/hello', (req, res) => {
+app.get('/api/hello', (req, res) => {
   return res.status(200).send('hello!');
 });
 
-exports.app = https.onRequest(app);
+exports.api = onRequest(app);
