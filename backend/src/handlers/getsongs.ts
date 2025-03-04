b969468d6df0a16e641c5e54fe6d4c6af7e73929
JaydenPahukula <jayden.pahukula@gmail.com>
2025-03-03 20:58:19 -0700

    fix backend issues

--- backend/src/handlers/getsongs.ts
+++ backend/src/handlers/getsongs.ts
@@ -1,28 +1,14 @@
 import { Handler, Request, Response } from 'express';
+import { isSongInfo } from 'shared/types/songinfo';
 import db from 'src/firebase/firestore';
 
 const getSongs: Handler = async (_request: Request, response: Response) => {
-  const collection = db.collection('song');
-  // console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS);
-  // console.log(process.env.FIRESTORE_EMULATOR_HOST);
-  // // console.log(await collection.doc('hello').get());
-  // console.log('hi');
-  // const test = await collection.add({ test: 'test' });
-  // console.log(test);
-  // const test = collection.get();
-  // console.log(await db.listCollections());
-  // const idk = await collection.get();
-  // console.log('--------------');
-  // console.log(idk);
-  // const songs = idk.docs
-  //   .filter((doc) => doc.exists)
-  //   .map((doc) => doc.data())
-  //   .filter(isSongInfo);
-  // console.log(songs);
-  // return response.status(200).send({ songs: songs });
-  return response
-    .status(200)
-    .send({ songs: [], test: (await collection.get()).docs.map((doc) => doc.data()) });
+  const songs = (await db.collection('song').get()).docs
+    .filter((doc) => doc.exists)
+    .map((doc) => doc.data())
+    .filter(isSongInfo);
+  console.log(songs);
+  return response.status(200).send({ songs: songs });
 };
 
 export default getSongs;
