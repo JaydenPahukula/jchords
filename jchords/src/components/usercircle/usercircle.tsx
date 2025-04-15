d99bfa325f85b40b3d5d9eeb84100dd318d5ffa5
JaydenPahukula <jayden.pahukula@gmail.com>
2025-04-15 16:20:45 -0600

    move expandablemenubutton to shared

--- jchords/src/components/usercircle/usercircle.tsx
+++ jchords/src/components/usercircle/usercircle.tsx
@@ -1,8 +1,8 @@
 import { useContext } from 'preact/hooks';
 import logOut from 'shared/auth/logout';
+import ExpandableMenuButton from 'shared/components/generic/expandablemenubutton';
 import UserIcon from 'shared/components/icons/usericon';
 import Dialog from 'shared/enums/dialog';
-import ExpandableMenuButton from 'src/components/expandablemenubutton/expandablemenubutton';
 import showDialog from 'src/state/functions/showdialog';
 import StateContext from 'src/state/statecontext';
 
