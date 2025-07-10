6c6ac6afe65a60b1b5d7b4892a31d6fe4897dda2
JaydenPahukula <jayden.pahukula@gmail.com>
2025-07-10 12:27:58 -1000

    add engine build script

--- engine/vitest.config.js
+++ engine/vitest.config.js
@@ -2,9 +2,6 @@ import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    environment: 'jsdom',
-  },
   resolve: {
     alias: {
       src: path.resolve(__dirname, './src'),
