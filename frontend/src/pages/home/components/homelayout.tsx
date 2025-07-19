308223bb1af7de1c1258b26a9e53be24d6ddd1f5
JaydenPahukula <jayden.pahukula@gmail.com>
2025-07-18 15:01:28 -1000

    add editor link in home header

--- frontend/src/pages/home/components/homelayout.tsx
+++ frontend/src/pages/home/components/homelayout.tsx
@@ -1,5 +1,5 @@
 import { useSignal, useSignalEffect } from '@preact/signals-react';
-import { Container, Flex, Grid, Heading } from '@radix-ui/themes';
+import { Container, Flex, Grid, Heading, Text } from '@radix-ui/themes';
 import { onAuthStateChanged, User } from 'firebase/auth';
 import { Link, Outlet } from 'react-router';
 import { CreateAccountDialog } from 'src/components/dialogs/createaccountdialog';
@@ -43,11 +43,21 @@ export function HomeLayout() {
               style={{ zIndex: '1', boxShadow: 'var(--shadow-4)' }}
             >
               <Flex height="100%" align="center" justify="between">
-                <Link to="/" style={{ color: 'var(--gray-12)', textDecorationLine: 'none' }}>
-                  <Heading size={{ initial: '6', sm: '7' }} mx="4">
-                    JChords
-                  </Heading>
-                </Link>
+                <Flex align="center" gap={{ sm: '2' }}>
+                  <Link to="/" style={{ color: 'var(--gray-12)', textDecorationLine: 'none' }}>
+                    <Heading size={{ initial: '6', sm: '7' }} mx="4">
+                      JChords
+                    </Heading>
+                  </Link>
+                  <Link
+                    to="/editor"
+                    style={{ color: 'var(--gray-12)', textDecorationLine: 'none' }}
+                  >
+                    <Text size="3" mx="4">
+                      Editor
+                    </Text>
+                  </Link>
+                </Flex>
                 <UserCircle
                   user={userSignal.value ?? null}
                   openLoginDialog={() => (dialogSignal.value = DialogType.Login)}
